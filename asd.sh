e2d950720829f50dd2aa581fed0486d9c1371707
Andrzej Skrzydlo <skrzydllo@gmail.com>
2019-01-16 15:08:02 +0100

    Update asd.sh

--- asd.sh
+++ asd.sh
@@ -1,3 +1,6 @@
+#0
 find . -printf %s %pn |sort -nr | head -n 3
-find / -type f -links +1 -printf %i %n %pn | head
+#1
 find -type f -regex ".*/.*\.\(shtml\|css\)
+#2
+find / -type f -links +1 -printf %i %n %pn | head
