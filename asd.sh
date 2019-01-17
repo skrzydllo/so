34f64e1ae7bd9cf5b8be4ed0133ae01884cdde0e
Andrzej Skrzydlo <skrzydllo@gmail.com>
2019-01-17 07:24:34 +0100

    Update asd.sh

--- asd.sh
+++ asd.sh
@@ -1,6 +1,8 @@
 #0
 find . -printf %s %pn |sort -nr | head -n 3
 #1
-find -type f -regex ".*/.*\.\(shtml\|css\)
+find -type f -regex ".*/.*\.\(shtml\|css\) -exec sh -c 'mv "$0" "${0%.txt}.COPY"' {} \;
+find . -type f -name "*.sh" -exec sh -c 'cp "$1" "${1%.sh}.edefg"' _ '{}' \;
+
 #2
 find / -type f -links +1 -printf %i %n %pn | head
